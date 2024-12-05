7fac044225252f35db3480651d261a565e5a98cc
gcronax <guillecampher15@gmail.com>
2024-12-05 20:52:53 +0100

    qweqweqwee

---
+++ src/main_contrasena.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class main_contrasena {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("dame el numero de contraseñas que quieras: ");
+        contrasena array[]=new contrasena[3];
+        System.out.println(Arrays.toString(array));
+
+
+
+
+
+    }
+}
