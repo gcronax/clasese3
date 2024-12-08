2f34017223975d32e8b281b47aa84a23e40553fc
gcronax <guillecampher15@gmail.com>
2024-12-08 22:07:19 +0100

    wqewqewqe

--- src/main_baraja.java
+++ src/main_baraja.java
@@ -1,38 +1,35 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class main_baraja {
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
 
-        System.out.println("Mostrar las cartas disponibles(" + baraja.cartasDisponibles()+")");
+        System.out.println("Mostrar las cartas disponibles (" + baraja.cartasDisponibles()+")");
 
         System.out.println("Sacar una carta: " + baraja.siguienteCarta());
 
         System.out.println("Sacar 8 cartas mas:");
-        List<Carta> cartasRepartidas = baraja.darCartas(8);
-        if (cartasRepartidas != null) {
-            for (Carta carta : cartasRepartidas) {
-                System.out.println(carta);
-            }
+        ArrayList<Carta> cartasRepartidas = baraja.darCartas(8);
+       
+        for (Carta carta : cartasRepartidas) {
+            System.out.println(carta);
         }
+        
+        System.out.println("Volver a mostrar las cartas disponibles (" + baraja.cartasDisponibles()+")");
 
-        System.out.println("Cartas disponibles: " + baraja.cartasDisponibles());
-
-        System.out.println("Cartas en el montón:");
         baraja.cartasMonton();
 
         System.out.println("Barajando la baraja...");
         baraja.barajar();
 
-        System.out.println("Saco 5 cartas después de barajar:");
+        System.out.println("Sacar 5 cartas:");
         cartasRepartidas = baraja.darCartas(5);
-        if (cartasRepartidas != null) {
-            for (Carta carta : cartasRepartidas) {
-                System.out.println(carta);
-            }
+        
+        for (Carta carta : cartasRepartidas) {
+            System.out.println(carta);
         }
-
-        System.out.println("Cartas en el montón después de barajar:");
+        
+        System.out.println("Mostrar las cartas sacadas después de barajar.");
         baraja.cartasMonton();
     }
 }
